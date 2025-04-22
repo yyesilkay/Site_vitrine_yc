c1ba7ba97cba480795197f791eb21e812d33df23
Bridja <a.bridja77@gmail.com>
2025-04-22 11:36:30 +0200

    Modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './components/contact/contact.component';
+
 import { HomeComponent } from './components/home/home.component';
-import { DevisComponent } from './components/devis/devis.component';
+
 
 export const routes: Routes = [{
 
@@ -15,13 +15,6 @@ export const routes: Routes = [{
 {
     path: 'home',
     component: HomeComponent
-},
-{
-    path: 'contact',
-    component: ContactComponent
-},{
-    path: 'devis',
-    component: DevisComponent
 }
 
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { EnTeteComponent } from '../en-tete/en-tete.component';
-import { FooterComponent } from '../footer/footer.component';
+
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [EnTeteComponent, FooterComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

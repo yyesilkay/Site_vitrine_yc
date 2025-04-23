c02192bffbcdcb4a43f3a149f2fdebc8cd9a6f6c
Bridja <a.bridja77@gmail.com>
2025-04-23 10:44:07 +0200

    Ajout page contact

---
+++ src/app/components/estimate-request/estimate-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estimate-request',
+  imports: [],
+  templateUrl: './estimate-request.component.html',
+  styleUrl: './estimate-request.component.css'
+})
+export class EstimateRequestComponent {
+
+}

765cb7daea2b639ced01251af0b4f282cd0b8741
ab3d0 <159782986+Ab3d0@users.noreply.github.com>
2025-04-24 10:22:07 +0200

    feat: add  About_us page to the application #5

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })

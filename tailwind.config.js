dc4e97222b2d2e4c9746711afcd3d7edf58897c8
Bridja <a.bridja77@gmail.com>
2025-05-22 11:48:45 +0200

    Fix: Update style css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./apps/frontend/src/**/*.{html,ts}", 
+    "./libs/**/*.{html,ts}",              
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
